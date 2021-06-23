e0e8c7963a19b64cc4ffd0597a2f6fba00f40e14
EKhoroshko <xapoxa14@gmail.com>
2021-06-23 13:32:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ class Timer {
 }
 
 const timer = new Timer({
-    onTick: setClock, selector: '#timer-1', deadline: new Date('Jun 23, 2021 13:28'),
+    onTick: setClock, selector: '#timer-1', deadline: new Date('Jun 29, 2021'),
 });
 timer.start();
 
